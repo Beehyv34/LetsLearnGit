736717289f675c4332db7eaf202c8039d449deac
Raju <Ryderraj3498@gmail.com>
2022-09-15 13:06:03 +0530

    added message in Sum.java

---
+++ Sum.java
@@ -0,0 +1,18 @@
+public class Sum{
+
+
+/**
+ * @param args
+ */
+public static void main(String[] args) {
+    int a = 5;
+    int b = 6;
+
+        extracted(a, b);
+}
+
+private static void extracted(int a, int b) {
+    System.out.println("THe sum is " + (a+b));
+}
+
+}
